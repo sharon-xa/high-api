7018d9e2cc9455a3a0b4edea2dfe4291a59d8090
sharon <ali93456@gmail.com>
2025-04-24 19:27:27 +0300

    feat: add initial database models for User, Post, Comment, Tag, and Category

---
+++ internal/database/models.go
@@ -0,0 +1,79 @@
+package database
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name     string
+	Gender   string
+	Image    string
+	Bio      string
+	Email    string `gorm:"unique"`
+	Password string
+	Role     string `gorm:"default:'user'"`
+	Verified bool   `gorm:"default:false"`
+
+	Posts    []Post
+	Comments []Comment
+}
+
+type AccountVerificationOTP struct {
+	gorm.Model
+	UserID    uint `gorm:"index"`
+	OTP       string
+	Verified  bool `gorm:"default:false"`
+	ExpiresAt time.Time
+}
+
+type PasswordResetToken struct {
+	ID        uint   `gorm:"primaryKey"`
+	UserID    uint   `gorm:"index"`
+	Token     string `gorm:"unique"`
+	ExpiresAt time.Time
+}
+
+type Post struct {
+	gorm.Model
+	UserID     uint
+	CategoryID uint
+	Title      string
+	Content    string
+	Image      string
+
+	User     User
+	Category Category
+	Tags     []Tag `gorm:"many2many:post_tags;"`
+	Comments []Comment
+}
+
+type Comment struct {
+	gorm.Model
+	PostID  uint
+	UserID  uint
+	Content string
+
+	Post Post
+	User User
+}
+
+type Tag struct {
+	ID    uint   `gorm:"primaryKey"`
+	Name  string `gorm:"unique"`
+	Posts []Post `gorm:"many2many:post_tags;"`
+}
+
+type PostTag struct {
+	PostID uint `gorm:"primaryKey"`
+	TagID  uint `gorm:"primaryKey"`
+}
+
+type Category struct {
+	ID   uint   `gorm:"primaryKey"`
+	Name string `gorm:"unique"`
+
+	Posts []Post
+}
