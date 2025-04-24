681c45312de3d1d66659f435b919bcde7a37e0ac
sharon <ali93456@gmail.com>
2025-04-24 19:31:38 +0300

    feat: add all API routes

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -8,23 +8,80 @@ import (
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
-	r := gin.Default()
+	engine := gin.Default()
 
-	r.Use(cors.New(cors.Config{
+	engine.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:5173"}, // Add your frontend URL
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
 		AllowHeaders:     []string{"Accept", "Authorization", "Content-Type"},
 		AllowCredentials: true, // Enable cookies/auth
 	}))
 
-	r.GET("/", s.HelloWorldHandler)
+	s.registerPublicRoutes(engine)
+	s.registerUserRoutes(engine)
+	s.registerAdminRoutes(engine)
 
-	return r
+	return engine
 }
 
-func (s *Server) HelloWorldHandler(c *gin.Context) {
-	resp := make(map[string]string)
-	resp["message"] = "Hello World"
+func (s *Server) registerPublicRoutes(e *gin.Engine) {
+	auth := e.Group("/auth")
 
-	c.JSON(http.StatusOK, resp)
+	auth.POST("/register")
+	auth.POST("/verify-email")
+	auth.POST("/login")
+	auth.POST("/forgot-password")
+	auth.POST("/reset-password")
+
+	posts := e.Group("/posts")
+	posts.GET("")
+	posts.GET("/:id")
+	posts.GET("/:id/comments")
+
+	categories := e.Group("/categories")
+	categories.GET("")
+
+	users := e.Group("/users")
+	users.GET("/:id")
+
+	tags := e.Group("/tags")
+	tags.GET("")
+	tags.GET("/:name") // Retrieve all posts associated with a specific tag.
+}
+
+func (s *Server) registerUserRoutes(e *gin.Engine) {
+	users := e.Group("/users")
+	users.PUT("/:id")
+	users.DELETE("/:id")
+
+	posts := e.Group("/posts")
+	posts.POST("")
+	posts.PUT("/:id")
+	posts.DELETE("/:id")
+	posts.POST("/:id/comments")
+
+	comments := e.Group("/comments")
+	comments.PUT("/:id")
+	comments.DELETE("/:id")
+}
+
+func (s *Server) registerAdminRoutes(e *gin.Engine) {
+	admin := e.Group("/admin")
+	admin.GET("/users")               // retrieve all users
+	admin.POST("/users/{id}/ban")     // ban a user
+	admin.POST("/users/{id}/promote") // promote a user to admin
+
+	admin.DELETE("/posts/:id")
+
+	admin.GET("/comments")
+	admin.DELETE("/comments/:id")
+
+	admin.POST("/category")
+	admin.PUT("/category/:id")
+	admin.DELETE("/category/:id")
+
+	admin.PUT("/tags/:id")
+	admin.DELETE("/tags/:id")
+
+	admin.GET("/stat")
 }
