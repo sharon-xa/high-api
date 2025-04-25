3e8ca1da50d2879821763fe28bf2148276284dfd
sharon <ali93456@gmail.com>
2025-04-25 21:27:56 +0300

    feat: implement GenerateRandomOTP function for one-time password generation

---
+++ internal/utils/helpers.go
@@ -0,0 +1,25 @@
+package utils
+
+import "crypto/rand"
+
+var digits = []byte("1234567890")
+
+func GenerateRandomOTP() string {
+	const otpLength = 6
+	otp := make([]byte, otpLength)
+
+	for i := 0; i < otpLength; i++ {
+		randomByte := make([]byte, 1)
+
+		for {
+			_, err := rand.Read(randomByte)
+			if err == nil {
+				break
+			}
+		}
+
+		otp[i] = digits[randomByte[0]%byte(len(digits))]
+	}
+
+	return string(otp)
+}
